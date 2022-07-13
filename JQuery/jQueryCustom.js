e5e0e777c0257244f94a1d917f2ef9a72a9b83b4
Joao Avelas <jplinkas@gmail.com>
2022-07-13 15:41:10 -0300

    adicinando explicação de CSS e jQuery

--- JQuery/jQueryCustom.js
+++ JQuery/jQueryCustom.js
@@ -11,6 +11,19 @@ jQuery ( function ($) {
     
 })
 
+//Configuração de produtos
+
+// $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+// $('.featured-item:first h4').append('<span class = "badge bg-secondary">Novo</span>');
+
+// $('.featured-item:first h4').start('<span class = "badge bg-secondary">Novo</span>');
+
+// $('.featured-item:first h4').html('<span class = "badge bg-secondary">Novo</span>');
+
+// $('.featured-item:first h4').addClass('active');
+
+//No CSS só precisa selecionar o elemento com $('.classe tag') e depois adicinar a função $('.classe tag').toggleClass('active')
 
 // pode causar conflito com outras bibliotecas
 // console.log ( $('h4').text() );
