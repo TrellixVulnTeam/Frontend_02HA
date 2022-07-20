9acb6a64ea228e6fa2f6749fe6e974418f357963
Joao Avelas <jplinkas@gmail.com>
2022-07-19 21:34:21 -0300

    continuando jQuery/animação

--- modulo16jQuery/jQueryCustom.js
+++ modulo16jQuery/jQueryCustom.js
@@ -53,7 +53,8 @@ jQuery ( function ($) {
     function() {console.log($(this).find('h4').text() + ' - ' + $(this).find('h6').text() )},
    )
 
-//Manipulação de evento
+// Manipulação de evento
+
 
 /*   $ ('.featured-item a ').on( 'click', function(event) {
 *
@@ -76,4 +77,31 @@ jQuery ( function ($) {
  *            .show (slow, function(){
  *            console.log( $(this).find('h4').text() + 'Em estoque' )
  *            })
-*/ 
+*/
+
+//Animações
+
+const duracao = 1000 // 1000 milisegundos; 1000 ms = 1s
+
+$('.featured-item:nth(0)')
+    .hide(duracao)
+    .show(duracao)
+    .fadeIn(1000)
+    .fadeOut(3000) // fadeOut não faz o item sumir completamente, só fica invisible no CSS
+    .toggle(4000)  // funciona parecido com hide e show, removendo o elemento da pagina totalmente
+
+$('#form-submit ').on('click', function(e){
+        
+    e.preventDefault()
+
+        if( $('#email').val( ) != '' ){
+
+            $('#email').animate({
+                opacity: "toggle", //isso faz o campo de email 'sumir' quando é preenchido
+            }, duracao, function(){
+                console.log(
+                    $(this).val() //isso mostra no console o que foi escrito no campo preenchido.
+                )
+            })
+        }
+});
