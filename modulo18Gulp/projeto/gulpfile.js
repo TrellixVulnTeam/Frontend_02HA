d487f826f31b3f66710c6e9c152fa1ce70053e35
Joao Avelas <jplinkas@gmail.com>
2022-07-30 17:55:45 -0300

    minificando arquivos, seja o  que Deus quiser

---
+++ modulo18Gulp/projeto/gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require ('gulp')
+const concat = require ('gulp-concat')
+const cssmin = require ('gulp-cssmin')
+const rename = require ('gulp-rename')
+const uglify = require ('gulp-uglify')
+
+function tarefasCSS(cb) {
+    return gulp.src('./src/**/*.css')           // o * serve pra que pegue todo e qualquer arquivo com .css
+               .pipe(concat ('libs.css'))       // função .pipe alinhada depois do ()
+               .pipe(cssmin ())                 // primeiro concatena os arquivos; depois pega os arquivos concatenados
+               .pipe(rename({suffix: '.min'}))  // minifica com o cssmin e depois de minificados
+               .pipe(gulp.dest('./dist/css'))   // renomeia, nesse caso, adicionando o sufixo .min
+                                                // gulp.src - onde vai buscar
+                                                // gulp.dest - onde vai colocar
+}
+
+function tarefasJS( ){
+    return gulp.src('./src/**/*.js')
+               .pipe(concat('libs.js'))
+               .pipe(uglify())
+               .pipe(rename({ suffix: '.min'})) // libs.min.js
+               .pipe(gulp.dest('./dist/js'))
+}
+
+exports.styles = tarefasCSS
+exports.scripts = tarefasJS
