9d8a03821e82742b2599a4ed28da377e1104f07a
Joao Avelas <jplinkas@gmail.com>
2022-09-23 20:15:01 -0300

    modulo27 code together

--- modulo27/src/js/index.js
+++ modulo27/src/js/index.js
@@ -1,5 +1,45 @@
-import '../scss/styles.scss'
+//import '../scss/styles.scss'
 
 const elemRoot = document.getElementById('root')
 elemRoot.classList.add('container')
 
+//video
+
+/*
+const video = document.querySelector('#video')
+const button = document.querySelector('#btn-play')
+
+document.querySelector('.fa-pause').style.display + 'none'
+
+const play = (event) => {
+    if(video.pause) {
+        document.querySelector('.fa-play').style.display + 'none'
+        document.querySelector('.fa-pause').style.display + 'block'
+        video.play( )
+
+    } else {
+        document.querySelector('.fa-play').style.display + 'block'
+        document.querySelector('.fa-pause').style.display + 'none'
+        video.pause
+    }
+}
+
+button.addEventListener('click', (event) => {
+    play(event)
+})
+video.addEventListener('click', (event) => {
+    play(event)
+})
+
+// timeline 
+
+
+video.addEventListener('timeupdate', ( )=>{
+    let current = (video.currentTime / video.duration) * 100
+    if(video.ended) {
+        document.querySelector('.fa-play').style.display + 'block'
+        document.querySelector('.fa-pause').style.display + 'none'
+    }
+    document.querySelector ('.inner').style.width = `${current}%`
+})
+*/
