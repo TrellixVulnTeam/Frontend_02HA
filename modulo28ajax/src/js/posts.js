3d4198494fa05b74a1f7c9bb350f5595847c449e
Joao Avelas <jplinkas@gmail.com>
2022-10-16 21:52:30 -0300

    modulo 28 code together

--- modulo28ajax/src/js/posts.js
+++ modulo28ajax/src/js/posts.js
@@ -6,14 +6,38 @@ let _data = {
     userId: 5
 }
 
-fetch ('https://jsonplaceholder.typicode.com/posts', {
+const postContainer = document.getElementById('posts')
+let postMarkup= ''
+
+function setPost(data){
+    fetch ('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
-    body: json.stringify( _data),
-    headers: {
-        'Content-Type' : 'application/json; charset=UTF-8'
-    }
+    body: json.stringify( data),
+    headers: {'Content-Type' : 'application/json; charset=UTF-8'}
 })
 
 .then ( response => response.json())
 .then ( json => console.log(json))
 .catch( error => console.error(error))
+}
+
+function getPost ( ) {    
+    fetch ('https://jsonplaceholder.typicode.com/posts', {
+    headers: {'Content-Type' : 'application/json; charset=UTF-8'}
+})
+
+.then ( response => response.json())
+.then ( data => {
+    data.map(function(post){
+        postMarkup += `
+            <div class = "posts-item" id="post-${post.id}">
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            </div> 
+        `
+    })
+    postContainer.innerHTML  = postMarkup
+})
+.catch( error => console.error(error))
+
+}
